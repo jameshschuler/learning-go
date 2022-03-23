099601ee45d046d30ba765d50eaa9c62d7bb99fe
jamesschuler <jameshschuler1@gmail.com>
2022-03-22 22:34:16 -0700

    can't stop

--- src/main.go
+++ src/main.go
@@ -2,16 +2,32 @@ package main
 
 import "fmt"
 
-type Cake struct {
+type Option struct {
+	id    uint8
 	name  string
 	price float32
 }
 
-var cakes = []Cake{
-	{name: "Basic Cake", price: 10.00},
-	{name: "Two-Layer Cake", price: 12.00},
-	{name: "Three-Layer Cake", price: 15.00},
-	{name: "Cupcake", price: 3.75}}
+var cakes = []Option{
+	{id: 1, name: "Basic Cake", price: 10.00},
+	{id: 2, name: "Two-Layer Cake", price: 12.00},
+	{id: 3, name: "Three-Layer Cake", price: 15.00},
+	{id: 4, name: "Cupcake", price: 3.75}}
+
+var cakeFlavorOptions = []Option{
+	{id: 1, name: "Chocolate", price: 0.00},
+	{id: 2, name: "Vanilla", price: 0.00},
+	{id: 3, name: "Spice", price: 1.00},
+	{id: 4, name: "Coffee", price: 2.00},
+}
+
+var toppingOptions = []Option{
+	{id: 1, name: "None", price: 0.00},
+	{id: 2, name: "Vanilla", price: 0.00},
+	{id: 3, name: "Chocolate", price: 1.00},
+	{id: 4, name: "Strawberry", price: 2.00},
+	{id: 5, name: "Ganache", price: 5.00},
+}
 
 func main() {
 	fmt.Println("\nWelcome to OnlyCakes!")
@@ -37,34 +53,37 @@ func handleEnteredOption(option uint8) {
 	case 1:
 		handleOrder()
 	case 2:
-		printCakesMenu()
+		printCustomMenu(cakes)
 	default:
-		fmt.Printf("Option %v not supported.\n", option)
+		fmt.Printf("Option %v is not supported.\n", option)
 		break
 	}
 }
 
 func handleOrder() {
-	fmt.Print("\nEnter which cake you would like.")
-	printCakesMenu()
+	printCustomMenu(cakes)
 
 	// Get cake option
-	var selectedCakeOption uint8
+	var selectedCakeOptionId uint8
+	var selectedCakeFlavorId uint8
 
 	fmt.Print("\nCake Option: ")
-	fmt.Scan(&selectedCakeOption)
+	fmt.Scan(&selectedCakeOptionId)
 
 	// Get cake flavor
+	printCustomMenu(cakeFlavorOptions)
+	fmt.Print("\nCake Flavor: ")
+	fmt.Scan(&selectedCakeFlavorId)
 
 	// Get frosting flavor
 
 	// Calculate cost
 }
 
-func printCakesMenu() {
+func printCustomMenu(options []Option) {
 	fmt.Println("\nOur current offerings:")
-	for i, cake := range cakes {
-		fmt.Printf("%d. %s $%.2f\n", i+1, cake.name, cake.price)
+	for _, cake := range options {
+		fmt.Printf("%d. %s $%.2f\n", cake.id, cake.name, cake.price)
 	}
 }
 
